284be4cfd92b704731c4b1fb8702042804d2bf85
SufranjNaweed <sufranj.naweed@gmail.com>
2020-02-10 00:31:35 +0100

    add scss, wip hero section

---
+++ src/Components/About/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function index() {
+    return (
+        <section id="about">
+            <h2>A Propos</h2>
+            <div className="part one">
+                <h3>Qui suis-je</h3> 
+                <p></p>
+            </div>
+            <div className="part two">
+                <h3>Formation & Expériences</h3> 
+                <p></p>
+            </div>
+            <div className="part three">
+                <h3>Stack Technique</h3> 
+                <p></p>
+            </div>
+        </section>
+    )
+}
---
+++ src/Components/Hero/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function index() {
+    return (
+        <>
+          <section id="hero">
+            <div className="container">
+              <div className="hero-content">
+                <h1>SUFRANJ Naweed</h1>
+                <h2>FullStack React/NodeJS</h2>
+                <p>
+                  <span>M</span>angodb <span>E</span>xpressJS <span>R</span>eactJS <span>N</span>odeJS 
+                </p>
+                <div className="container-btn">
+                  <a className="btn-empty btn-gradient-border" href="" download>CV</a>
+                  <a className="btn-full" href="" >PROJET</a> 
+                </div>
+              </div>
+            </div>
+          </section>
+        </>
+    )
+}
