7bc661689569e42675b6b7374dc26a17fbe21de6
NaweedSufranj <sufranj.naweed@gmail.com>
2020-02-16 17:16:35 +0100

    Updates

---
+++ precache-manifest.09cb1dabe5457b69948da0c41e680a9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22ff927b4273886d8c06a7213768194",
+    "url": "/naweed-sufranj/index.html"
+  },
+  {
+    "revision": "46983d4cf93b483d2a73",
+    "url": "/naweed-sufranj/static/css/main.fe65983a.chunk.css"
+  },
+  {
+    "revision": "cbad69058e51c9534436",
+    "url": "/naweed-sufranj/static/js/2.d0eeaf18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/naweed-sufranj/static/js/2.d0eeaf18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "46983d4cf93b483d2a73",
+    "url": "/naweed-sufranj/static/js/main.84400b47.chunk.js"
+  },
+  {
+    "revision": "19f97c77e8237df0e215",
+    "url": "/naweed-sufranj/static/js/runtime-main.44831495.js"
+  }
+]);
