9cb5880eb2792e7e83f2e3f8fd437ba48b2a9167
NaweedSufranj <sufranj.naweed@gmail.com>
2020-02-16 17:06:05 +0100

    add datas

---
+++ src/Datas/index.js
@@ -0,0 +1,14 @@
+const aboutDatas = {
+    formations : [
+        {logo : "../../Assets/images/epitech_logo.png", altImg : "logo Epitech", title : "Epitech", level : "Bac +1", techs: "C / JS"},
+        {logo : "../../Assets/images/logo_academy.png", altImg : "logo 3W Academy", title : "3W Academy", level : "Bac +2", techs: "HTML / CSS / JS / PHP/MySQL"},
+        {logo : "../../Assets/images/ipssi_logo.png", altImg : "logo IPSSI", title : "IPSSI", level : "Bac +4", techs: "MERN Stack & PHP Symfony"},
+    ],
+    experiences : [
+        {logo : "../../Assets/images/epresspack_logo.png", altImg : "logo ePressPack", title : "ePressPack", jobTitle: "Developpeur Front-End", year : "2018 - 2019"},
+        {logo : "../../Assets/images/adn_realty_logo.png", altImg : "logo ADN Realty", title : "ADN Realty", jobTitle: "Developpeur Fullstack PHP", year : "Janvier 2019 - Aout 2019"},
+        {logo : "../../Assets/images/digitalcube_logo.png", altImg : "logo DigitalCube", title : "DigitalCube", jobTitle: "Developpeur ReactJS/Native", year : "Septembre 2019 - Mars 2020"}
+    ]
+}
+
+export default aboutDatas;
